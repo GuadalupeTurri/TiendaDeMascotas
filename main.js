f80851e23835170b70fe9e877a1d1360cab35554
guadalupeturri <magaliturri@gmail.com>
2023-11-28 18:31:38 -0300

    comienzo de pagina

---
+++ main.js
@@ -0,0 +1,12 @@
+//actividad de clase 1
+let nombre = prompt("ingresar nombre");
+alert ("Hola " + "  " + nombre + " como estas?");
+console.log (nombre);
+
+let edad = Number(prompt ("ingresar edad"));
+alert ("tienes" + "  " + edad + " años?");
+console.log (edad);
+
+let año = Number (prompt ("ingrese año de nacimiento"));
+alert ("naciste en " +  "  " + año + " ?");
+console.log (año);
