cc371dbce9125b50dd28ad5cd8aff5f1c247b4ec
guadalupeturri <magaliturri@gmail.com>
2023-11-29 20:05:48 -0300

    aplicando carrucel

---
+++ js/main.js
@@ -0,0 +1,60 @@
+//actividad de clase 1
+// let nombre = prompt("ingresar nombre");
+// alert ("Hola " + "  " + nombre + " como estas?");
+// console.log (nombre);
+
+// let edad = Number(prompt ("ingresar edad"));
+// alert ("tienes" + "  " + edad + " años?");
+// console.log (edad);
+
+// let año = Number (prompt ("ingrese año de nacimiento"));
+// alert ("naciste en " +  "  " + año + " ?");
+// console.log (año);
+
+//actividad clase 2
+// let respuesta = prompt ("Terminaste la tarea?");
+//    if(respuesta === "si"){
+//  alert ("puedes salir a jugar");
+//   } else {
+//    alert ("no puedes salir a jugar");
+//   }
+
+// let edad = Number(prompt("ingrese su edad"));
+//     if (edad < 14){
+//         alert("no puedes entrar");
+//     } else if (edad < 18) {
+//         alert ("ingresar con un adulto responsable");
+//     } else {
+//         alert ("puedes entrar");
+//     }
+
+// // let nombre = prompt ("ingrese el nombre");
+// // let apellido = prompt ("ingrese el apellido");
+
+// // if (nombre != " " && apellido != " ") {
+// //     alert ("hola  "  + nombre + " " + apellido);
+// // } else {
+// //     alert(" los datos son requeridos")
+// // }
+
+//actividad de clase 2
+
+// let numero = prompt("ingrese su numero");
+//  if (numero > 1000) {
+//     alert("Excede la cantidad")
+//  } else {
+//     alert("el numero no es mayor a 1000")
+//  }
+
+//  let texto = prompt("ingrese el texto");
+//  if (texto === "hola"){
+//     console.log("hola");
+//  }else {
+//     console.log("no valido el texto");
+// //  }
+//  let Numero = prompt ("ingrese un numero");
+//   if (Numero >=10 && Numero <=50) {
+//     alert("Numero aprobado");
+//   }else {
+//     alert("numero invalido");
+//   }
